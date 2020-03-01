36f07b5ac8189980d68d170b3bb9c7635a177a20
Ervin Notari Junior <ervinnotari@hotmail.com>
2020-03-01 20:24:25 -0300

    Ajustes de estilo

---
+++ Bowling.Application.PainelOnBlazor/Data/Game/Frame.razor.cs
@@ -0,0 +1,27 @@
+﻿using Bowling.Domain.Game.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNetCore.Components.Web
+{
+    public class FrameBase : ComponentBase
+    {
+        [Parameter] public Player Player { get; set; }
+        [Parameter] public int FrameId { get; set; }
+        protected Frame Frame => Player.Frames.ElementAtOrDefault(FrameId);
+        protected string Attempt1 => AttNormalize(Frame.Balls.ElementAtOrDefault(0), 1);
+        protected string Attempt2 => (Frame.Balls.Count > 1) ? AttNormalize(Frame.Balls.ElementAtOrDefault(1), 2) : null;
+        protected string Attempt3 => (Frame.Balls.Count > 2) ? AttNormalize(Frame.Balls.ElementAtOrDefault(2), 3) : null;
+        protected string Score => Frame?.Score.ToString();
+
+        private string AttNormalize(int? value, int id)
+        {
+            if (value == 10) return "X";
+            else if (value == 0) return "-";
+            else if ((id == 2 && Frame.Balls.ElementAtOrDefault(1) + value == 10) || (id == 3 && Frame.Balls.ElementAtOrDefault(2) + value == 10)) return "/";
+            else return $"{value}";
+        }
+    }
+}
---
+++ Bowling.Application.PainelOnBlazor/Data/Game/Game.razor.cs
@@ -0,0 +1,38 @@
+﻿using Bowling.Domain.Game.Entities;
+using BowlingPainelOnBlazor.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNetCore.Components.Web
+{
+    public class GameBase : ComponentBase, IDisposable
+    {
+        [Parameter] public string Alley { get; set; } = "01";
+        [Inject] protected BowlingService BowlingService { get; set; }
+        protected Painel Painel { get; set; }
+
+        protected override Task OnParametersSetAsync()
+        {
+            BowlingService.OnChange += BowlingService_OnChange;
+            BowlingService_OnChange(null, EventArgs.Empty);
+            return base.OnParametersSetAsync();
+        }
+
+        private void BowlingService_OnChange(object sender, EventArgs e) => InvokeAsync(RefrashOnChange);
+
+        private async void RefrashOnChange()
+        {
+            Painel = await BowlingService.GetScoreAsync(Alley);
+            StateHasChanged();
+        }
+
+        public void Dispose()
+        {
+            BowlingService.OnChange -= BowlingService_OnChange;
+        }
+    }
+}
