93a64f54c751ddd953c7f0476f5662046ed55f9e
Ervin Notari Junior <ervin@inovadora.com.br>
2020-03-31 13:39:51 -0300

    Ajustes de code smells

--- Bowling.Application.PainelOnBlazor/Program.cs
+++ Bowling.Application.PainelOnBlazor/Program.cs
@@ -5,12 +5,16 @@ namespace Bowling.Application.PainelOnBlazor
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            var host = (args != null) ? Host.CreateDefaultBuilder(args) : Host.CreateDefaultBuilder();
-            host.ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
-                .Build()
-                .Run();
+            CreateHostBuilder(args).Build().Run();
         }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
