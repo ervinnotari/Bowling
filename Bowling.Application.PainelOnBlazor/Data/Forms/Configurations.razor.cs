9b7daded17110f81eab2d14f2706a936c7406c4b
Ervin Notari Junior <ervinnotari@hotmail.com>
2021-04-23 20:53:23 -0300

    add pagina de configurações

---
+++ Bowling.Application.PainelOnBlazor/Data/Forms/Configurations.razor.cs
@@ -0,0 +1,23 @@
+﻿using BowlingPainelOnBlazor.Data;
+using Bowling.Domain.Game.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.AspNetCore.Components.Web
+{
+    public class ConfigurationsBase : ComponentBase
+    {
+        [Inject]
+        protected IBusService BusService { get; set; }
+        [Inject]
+        protected IConfiguration Configuration { get; set; }
+
+        protected ConfigurationsModel Model;
+
+        protected override void OnInitialized()
+        {
+            Model = new ConfigurationsModel(Configuration, BusService);
+            Model.OnFormStateChange += () => StateHasChanged();
+        }
+
+    }
+}
